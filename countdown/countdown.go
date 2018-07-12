6400a94e447f5b95f557853ef7cd17943204f861
Michael Madden <mikemadden42@icloud.com>
2018-07-12 13:41:33 -0500

    Update start date

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	start := time.Date(2016, 3, 30, 14, 0, 0, 0, time.UTC)
+	start := time.Date(2018, 3, 5, 14, 0, 0, 0, time.UTC)
 	end := time.Now().UTC()
 	fmt.Println("diff:", DiffDays(start, end), "days")
 }
